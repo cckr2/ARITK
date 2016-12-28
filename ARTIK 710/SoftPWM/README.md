cb291355cebc9729d38ba8f44aa047ab499bcc18
JSY <cckr2@naver.com>
2016-12-28 17:08:32 +0900

    add Explanation

--- ARTIK 710/SoftPWM/README.md
+++ ARTIK 710/SoftPWM/README.md
@@ -1,3 +1,5 @@
 ## ARTIK SoftPWM library
- - duty_cycle : 0~100 (%) 
+ - Constructor's parameters are 'Pin Number' and 'Duty Cycle'
+ - 'Duty Cycle' is 0 to 100
+ - also Function 'setDuty' has parameter that Duty Cycle(0 to 100).
 
--- ARTIK 710/SoftPWM/Sample/LED_Test/LED_Test.ino
+++ ARTIK 710/SoftPWM/Sample/LED_Test/LED_Test.ino
@@ -1,3 +1,8 @@
+/*  Constructor's parameters are 'Pin Number' and 'Duty Cycle'
+ *  'Duty Cycle' is 0 to 100
+ *  also Function 'setDuty' has parameter that Duty Cycle(0 to 100).
+ */
+
 #include <DebugSerial.h>
 #include <SoftPWM.h>
 #define R 2
@@ -10,7 +15,7 @@ void setColor(int color_R, int color_G,int color_B){
     int color_Red = (color_R + 1) * 100 / 255;
     int color_Green = (color_G + 1) * 100 / 255;
     int color_Blue = (color_B + 1) * 100 / 255;
-    Red.setDuty(color_Red);  //Change Duty Cycle
+    Red.setDuty(color_Red);  //Change Duty Cycle(0~100(%))
     Green.setDuty(color_Green);
     Blue.setDuty(color_Blue);
 }
@@ -18,7 +23,7 @@ void setColor(int color_R, int color_G,int color_B){
 void setup() {
   // put your setup code here, to run once:
   DebugSerial.begin(115200);
-  Red = SoftPWM(R, 0);    //Init Pin Number and Duty Cycle
+  Red = SoftPWM(R, 0);    //Init Pin Number and Duty Cycle(0~100(%))
   Green = SoftPWM(G, 0);
   Blue = SoftPWM(B, 0);
 }
